1f40bfa1374b6ca737d6d0fbcf41062e7b9b43a8
lijian <lijian@ijunhai.com>
2018-09-14 12:06:23 +0800

    第一次提交代码

---
+++ src/component.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lijian
+ * Date: 2018/9/14
+ * Time: 11:59
+ */
+namespace Lijian\PHP;
+
+class info
+{
+    public static function info()
+    {
+        echo 'hello world';
+    }
+}
